b8d57c19ad2a0d48d6672d225a57911fc8679055
Museus <museus@protonmail.com>
2022-04-05 22:58:11 -0400

    namespace: update from DeathsDoorSaveManager to TunicSaveManager

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Windows.Forms;
 
-namespace DeathsDoorSaveManager {
+namespace TunicSaveManager {
     static class Program {
         /// <summary>
         ///  The main entry point for the application.
